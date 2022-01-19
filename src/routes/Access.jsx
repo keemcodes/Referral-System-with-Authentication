25b19cbd47bd2f2c5868a98a03996f5bf64fd338
Anderson <Akeem.Anderson@atl.com>
2022-01-19 15:08:24 -0500

    frontend auth working :)

--- src/routes/Access.jsx
+++ src/routes/Access.jsx
@@ -1,13 +1,17 @@
-import Home from "../components/Home";
+import  { useContext } from 'react';
+import { AuthContext } from '../Auth';
 
+
+import Home from "../components/Home";
 import AccessForm from "../components/AccessForm";
 
 export default function Access() {
-
+  const { isAuth, logout } = useContext(AuthContext);
+  console.log("App auth: ", isAuth);
     return (
       <>
         <main>
-          <AccessForm />
+          {isAuth ? (<Home />) : (<AccessForm />)}
         </main>
       </>
     );
